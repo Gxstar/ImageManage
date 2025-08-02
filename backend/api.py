655cab28cb8b3ddc503354a5cbe402da6d15b0d4
gxstar <gengxing123@qq.com>
2025-08-03 01:50:18 +0800

    feat: 实现后端API和前端窗口初始化
    refactor(前端): 移除Sidebar组件中的冗余代码

---
+++ backend/api.py
@@ -0,0 +1,7 @@
+import webview
+
+class Api:
+    def __init__(self):
+        self.window=None
+    def test_log(self):
+        return "这是一条来自后端的日志"
--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,15 @@
-def main():
-    print("Hello from imagemanage!")
+import webview
+from api import Api
 
+def main():
+    window=webview.create_window(
+        title='测试窗口',
+        url='http://localhost:5173',
+        width=1200,
+        height=800,
+        js_api=Api()
+        )
+    webview.start(debug=True)
 
 if __name__ == "__main__":
     main()
